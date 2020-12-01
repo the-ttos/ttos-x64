da2af510f7e149740764356602bc6f0266cda213
Thiago <thiagozoid@gmail.com>
2020-12-01 12:46:08 -0300

    Add basic EFI API

---
+++ gnu-efi/bootloader/main.c
@@ -0,0 +1,8 @@
+#include <efi.h>
+#include <efilib.h>
+
+EFI_STATUS efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
+	InitializeLib(ImageHandle, SystemTable);
+	Print(L"Test string.\n\r");
+	return EFI_SUCCESS; // Exit the UEFI application
+}
